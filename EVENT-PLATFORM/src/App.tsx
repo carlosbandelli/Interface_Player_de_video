8a9b7860efb51e8f64060ba764284ae9e54880e4
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2022-06-27 23:59:18 -0300

    Estrutura de homepage

--- EVENT-PLATFORM/src/App.tsx
+++ EVENT-PLATFORM/src/App.tsx
@@ -1,22 +1,32 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-
-const GET_LESSONS_QUERY = gql `
-query {
-  lessons {
-    id
-    title
-  }
-}
-`
-interface Lesson {
-  id: string;
-  title: string;
+ import { gql, useQuery } from "@apollo/client";
+// import { useEffect } from "react";
+import { Event } from "./pages/Event"
+function App(){
+  return (
+      <header>
+          <Event/>
+      </header>
+  )
 }
 
-function App() {
+export default App
 
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+// const GET_LESSONS_QUERY = gql `
+// query {
+//   lessons {
+//     id
+//     title
+//   }
+// }
+// `
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
+
+// function App() {
+
+  // const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   // useEffect(() => {
   //   client.query({
@@ -26,13 +36,15 @@ function App() {
   //   })
   // },[])
   
-  return (
-   <ul>
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-   </ul>
-  )
-}
+  // return (
+  //  <ul>
+  //   {data?.lessons.map(lesson => {
+  //     return <li key={lesson.id}>{lesson.title}</li>
+  //   })}
+  //  </ul>
+  // )
+// }
 
-export default App
+// export default App
+
+
